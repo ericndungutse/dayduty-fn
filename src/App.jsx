3441ac1dde5065fe55d7cfc55677b86a95f37d05
Eric Ndungutse <dav.ndungutse@gmail.com>
2023-09-01 16:57:01 +0300

    dashboard layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import AuthRoot from './pages/auth/AuthRoot';
 import Signin from './pages/auth/Signin';
 import Signup from './pages/auth/Signup';
 import './App.css'
+import DashboardRoot from './pages/dashboard/DashboardRoot';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
+    <Route>
     <Route path='/' element={<AuthRoot />}>
       <Route index element={<Signin />} />
       <Route path='signin' element={<Signin />} />
       <Route path='signup' element={<Signup />} />
+    </Route>
+    <Route path='/dashboard' element={<DashboardRoot/>} />
+
     </Route>
   )
 )
